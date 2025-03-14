f4197010b46bb8fcc9122db2e2155308582b4f4f
UshakovOleksii <ushakovalex.05@gmail.com>
2025-03-14 16:38:38 +0100

    done homework2

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+                  let orderSum = quantity * pricePerDroid;
+                  if (orderSum > customerCredits)
+                  {
+                  return "Insufficient funds!";
+                  }
+                  else {
+                           return `You ordered ${quantity} droids worth ${orderSum} credits!`                          
+                  }
+                  
+
+}
+
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000)); 
+console.log(makeTransaction(10, 5000, 8000)); 
+console.log(makeTransaction(8, 2000, 10000)); 
+console.log(makeTransaction(10, 500, 5000)); 
